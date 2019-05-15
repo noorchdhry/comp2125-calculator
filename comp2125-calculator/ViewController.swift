aa4d025efa18c0617d4d1db088ffb58d7d92ad82
Noor <noor.m.chaudhry@gmail.com>
2019-05-15 15:23:41 -0400

    Initial Commit

---
+++ comp2125-calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  comp2125-calculator
+//
+//  Created by Noor Chaudhry on 2019-05-15.
+//  Copyright © 2019 Noor Chaudhry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ comp2125-calculatorTests/comp2125_calculatorTests.swift
@@ -0,0 +1,34 @@
+//
+//  comp2125_calculatorTests.swift
+//  comp2125-calculatorTests
+//
+//  Created by Noor Chaudhry on 2019-05-15.
+//  Copyright © 2019 Noor Chaudhry. All rights reserved.
+//
+
+import XCTest
+@testable import comp2125_calculator
+
+class comp2125_calculatorTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
