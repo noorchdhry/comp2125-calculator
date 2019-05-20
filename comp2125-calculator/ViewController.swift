bd5c4f0b3467296ccaa0d4db1201d5ac8b5770ca
Noor <noor.m.chaudhry@gmail.com>
2019-05-20 18:14:17 -0400

    Changes
    Upload whatever changes were made to the calculator

--- comp2125-calculator/ViewController.swift
+++ comp2125-calculator/ViewController.swift
@@ -8,13 +8,39 @@
 
 import UIKit
 
+
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var displayLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        let buttonPressed = sender.currentTitle!
+        print("You just pressed: " + buttonPressed)
+        
+        var currentValue = displayLabel.text! //+ buttonPressed
+    
+        switch (sender.currentTitle) {
+        case "0","1","2","3","4","5","6","7","8","9":
+            currentValue = currentValue + buttonPressed
+        
+            if(currentValue == "0"){
+                currentValue = buttonPressed
+            } else {
+                currentValue += buttonPressed
+            }
+            break
+        default:
+            break
+    
+        }
+    }
+    
 }
 
